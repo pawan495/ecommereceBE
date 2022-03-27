568ca1aa1f816e93b12c4493ac97d6f76d671685
pawan495 <pawanpreetkaur495@gmail.com>
2022-03-27 18:21:12 -0400

    make it better

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -2,28 +2,28 @@ const { Tag } = require('../models');
 
 const tagData = [
   {
-    tag_name: 'rock music',
+    tag_name: 'Jazz music',
   },
   {
-    tag_name: 'pop music',
+    tag_name: 'Classical music',
   },
   {
-    tag_name: 'blue',
+    tag_name: 'peach',
   },
   {
-    tag_name: 'red',
+    tag_name: 'purple',
   },
   {
-    tag_name: 'green',
+    tag_name: 'sea-green',
   },
   {
     tag_name: 'white',
   },
   {
-    tag_name: 'gold',
+    tag_name: 'black',
   },
   {
-    tag_name: 'pop culture',
+    tag_name: 'indian culture',
   },
 ];
 
